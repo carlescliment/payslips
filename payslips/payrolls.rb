e7f8d6650a8dc640551b40c7308091a81a2186c6
carlescliment <carlescliment@gmail.com>
2019-02-06 22:13:14 +0100

    Write payslips to file

--- payslips/payrolls.rb
+++ payslips/payrolls.rb
@@ -16,6 +16,25 @@ class Payrolls
     Payroll.new(month, year, lines.map { |line| as_payslip(line) })
   end
 
+  def store(payroll)
+    File.open(path_for(payroll.month, payroll.year)) do |file|
+      payroll.payslips.each do |payslip|
+        file.write(
+          payslip.id.to_s.rjust(12, '0') +
+          payslip.vat.rjust(9) +
+          payslip.date.strftime('%Y%m%d') +
+          payslip.gross.to_s.rjust(8, '0') +
+          (payslip.deductions * 100).to_i.to_s.rjust(4, '0') +
+          payslip.amount_of_deductions.to_s.rjust(8, '0') +
+          (payslip.irpf * 100).to_i.to_s.rjust(4, '0') +
+          payslip.amount_of_irpf.to_s.rjust(8, '0') +
+          payslip.net.to_s.rjust(8, '0') +
+          "\n"
+        )
+      end
+    end
+  end
+
   private
 
   def path_for(month, year)
@@ -30,7 +49,7 @@ class Payrolls
       vat: line[12..20],
       date: Date.parse("#{line[21..24]}-#{line[25..26]}-#{line[27..28]}"),
       gross: line[29..36].to_i,
-      deductions: "#{line[37..38]}.#{line[39..40]}".to_f,
+      deductions: "#{line[37..40]}".to_f / 100,
       irpf: "#{line[49..50]}.#{line[51..52]}".to_f
     )
   end
--- spec/payslips/payrolls_spec.rb
+++ spec/payslips/payrolls_spec.rb
@@ -2,8 +2,11 @@ require_relative '../../payslips/payrolls'
 
 
 RSpec.describe Payrolls do
+
+  let (:fixtures_path) { "#{File.dirname(__FILE__)}/fixtures" }
+
   before(:each) do
-    @payrolls = Payrolls.new("#{File.dirname(__FILE__)}/fixtures")
+    @payrolls = Payrolls.new(fixtures_path)
   end
 
   context 'when loading payrolls' do
@@ -32,6 +35,22 @@ RSpec.describe Payrolls do
 
         expect(payslips_read).to be_empty
       end
+    end
+  end
+
+  context 'when storing payrolls' do
+    it 'writes the contents' do
+      file = StringIO.new
+      payroll = Payroll.new(12, 2018, [
+        Payslip.new(id: 1, vat: '97084172E', date: Date.parse('2018-12-31'),
+                    gross: 248600, deductions: 5.0, irpf: 12.0)
+      ])
+      expect(File).to receive(:open).with("#{fixtures_path}/payslips.201812.txt").and_yield(file)
+
+      expected_output = "00000000000197084172E201812310024860005000001243012000002983200206338\n"
+      expect(file).to receive(:write).with(expected_output)
+
+      @payrolls.store(payroll)
     end
   end
 end
